d23276e2deebf81f06da622adf9ed86dafeb4833
Rico Derks <r.j.e.derks@lumc.nl>
2021-11-24 10:13:05 +0100

    adjusted for local running

--- examples/config_script.R
+++ examples/config_script.R
@@ -8,8 +8,8 @@ DEV_OMICSER <- TRUE
 if (DEV_OMICSER){
   # this should be a full path... e.g. ~/Projects/NDCN_dev/omicser
   # but for github, we will set relative to the repo BASE
-  REPO_PATH <- "/Users/ahenrie/Projects/NDCN_dev/omicser"
-  OMICSER_RUN_DIR <- file.path(REPO_PATH,"quickstart")
+  REPO_PATH <- "/home/ricoderks/Documents/LUMC/Projects/omicser/" # "/Users/ahenrie/Projects/NDCN_dev/omicser"
+  OMICSER_RUN_DIR <- file.path(REPO_PATH) #file.path(REPO_PATH,"quickstart")
   golem::document_and_reload(pkg = REPO_PATH)
 } else {
 
@@ -21,13 +21,13 @@ if (DEV_OMICSER){
 db_root_path <- "test_db"
 
 database_names <- list(
-  "Domenico DIA" = "domenico_stem_cell",
-  "Vilas Microglia (sceasy)" = "vilas_microglia_sceasy",
-  "Yassene Lipid concentraions & compositions" ="yassene_lipid"
+  "Domenico DIA" = "domenico_stem_cell"
+  # "Vilas Microglia (sceasy)" = "vilas_microglia_sceasy",
+  # "Yassene Lipid concentraions & compositions" ="yassene_lipid"
 )
 
 
-conda_environment = 'omxr'
+conda_environment = ".pyenv" # 'omxr'
 omicser_options <- list(database_names=database_names,
                         db_root_path=db_root_path,
                         conda_environment=conda_environment)
